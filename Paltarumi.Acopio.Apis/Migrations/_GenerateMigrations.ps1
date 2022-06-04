83650c797fa779fb47836f15cccc2bbff02f4439
Nelsson José <njose29@outlook.com>
2022-06-03 22:31:29 -0500

    Adding security context and features

---
+++ Paltarumi.Acopio.Apis/Migrations/_GenerateMigrations.ps1
@@ -0,0 +1,5 @@
+﻿dotnet ef migrations add AcopioMigration_00 --context AcopioDbContext
+dotnet ef database update --context AcopioDbContext
+
+dotnet ef migrations add SecurityMigration_00 --context SecurityDbContext
+dotnet ef database update --context SecurityDbContext
---
+++ Paltarumi.Acopio.Repository/Data/SecurityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Paltarumi.Acopio.Entity;
+
+namespace Paltarumi.Acopio.Repository.Data
+{
+    public class SecurityDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+    {
+        public SecurityDbContext(DbContextOptions<SecurityDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
