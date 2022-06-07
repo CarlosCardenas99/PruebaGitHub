37f6e820326cf3107e72f51dee4401fa431e0d2d
Walter Canchan <walter.canchan@somosoh.pe>
2022-06-07 18:54:01 -0500

    merge

--- Paltarumi.Acopio.Domain/Dto/Balanza/Lote/GetLoteDto.cs
+++ Paltarumi.Acopio.Domain/Dto/Balanza/Lote/GetLoteDto.cs
@@ -5,33 +5,13 @@ using Paltarumi.Acopio.Domain.Dto.Maestro.Concesion;
 
 namespace Paltarumi.Acopio.Domain.Dto.Balanza.Lote
 {
-    public class GetLoteDto
+    public class GetLoteDto: LoteDto
     {
         public int IdLote { get; set; }
-        public string? Codigo { get; set; }
-        public DateTime? FechaIngreso { get; set; }
-        public string? HoraIngreso { get; set; }
-        public DateTime? FechaAcopio { get; set; }
-        public string? HoraAcopio { get; set; }
-        public string? CantidadSacos { get; set; }
-        public float? Tmh100 { get; set; }
-        public float? TmhBase { get; set; }
-        public float? Tmh { get; set; }
-        public float? Humedad100 { get; set; }
-        public float? HumedadBase { get; set; }
-        public float? Humedad { get; set; }
-        public float Tms100 { get; set; }
-        public float TmsBase { get; set; }
-        public float? Tms { get; set; }
         public GetConcesionDto Concesion { get; set; }
         public GetMaestroDto EstadoTipoMaterial { get; set; }
         public GetProveedorDto Proveedor { get; set; }
         public GetMaestroDto Estado { get; set; }
-        public string? Observacion { get; set; }
-        public int? IdUsuarioCreate { get; set; }
-        public DateTime? CreateDate { get; set; }
-        public int? IdUsuarioUpdate { get; set; }
-        public DateTime? UpdateDate { get; set; }
         public bool Activo { get; set; }
         public IEnumerable<ListTicketDto>? TicketDetails { get; set; }
     }
