d2280a933490dad97af6b0dba575cb2dedf66a04
Walter Canchan <walter.canchan@somosoh.pe>
2022-06-08 18:22:08 -0500

    servcio consulta por placa

--- Paltarumi.Acopio.Application.Abstractions/Maestro/IVehiculoApplication.cs
+++ Paltarumi.Acopio.Application.Abstractions/Maestro/IVehiculoApplication.cs
@@ -9,6 +9,7 @@ namespace Paltarumi.Acopio.Application.Abstractions.Maestro
         Task<ResponseDto<GetVehiculoDto>> Update(UpdateVehiculoDto updateDto);
         Task<ResponseDto> Delete(int id);
         Task<ResponseDto<GetVehiculoDto>> Get(int id);
+        Task<ResponseDto<GetVehiculoDto>> GetByPlaca(string placa);
         Task<ResponseDto<IEnumerable<ListVehiculoDto>>> List();
         Task<ResponseDto<SearchResultDto<SearchVehiculoDto>>> Search(SearchParamsDto<SearchVehiculoFilterDto> searchParams);
     }
