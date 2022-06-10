e2a5ce7c6ab4fd2186daa61cbad8a358057dcbce
cesar <joyero.net@hotmail.com>
2022-06-09 23:09:26 -0500

    correcion actualizacion transporte

--- Paltarumi.Acopio.Domain/Dto/Maestro/Transporte/TransporteDto.cs
+++ Paltarumi.Acopio.Domain/Dto/Maestro/Transporte/TransporteDto.cs
@@ -5,7 +5,7 @@ namespace Paltarumi.Acopio.Domain.Dto.Maestro.Transporte
     {
         public string Ruc { get; set; }
         public string RazonSocial { get; set; }
-        public string CodigoUbigeo { get; set; }
+        public string? CodigoUbigeo { get; set; }
         public string Direccion { get; set; }
         public string Telefono { get; set; }
         public string Email { get; set; }
