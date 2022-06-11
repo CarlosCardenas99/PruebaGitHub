12091219f7bb78ecdc19f0226208b84d9f7f6709
Walter Canchan <walter.canchan@somosoh.pe>
2022-06-11 10:29:54 -0500

    serv ley ref por nodos

--- Paltarumi.Acopio.Domain/Dto/Balanza/Recodificacion/RecodificacionDto.cs
+++ Paltarumi.Acopio.Domain/Dto/Balanza/Recodificacion/RecodificacionDto.cs
@@ -3,10 +3,10 @@ namespace Paltarumi.Acopio.Domain.Dto.Balanza.Recodificacion
 {
     public class RecodificacionDto
     {
-		public int IdLote { get; set; }
-		public DateTime FechaRecodificacion { get; set; }
-		public string HoraRecodificacion { get; set; }
-		public string Codigo { get; set; }
-		public string CodigoLaboratorio { get; set; }
+        public int IdLote { get; set; }
+        public DateTime FechaRecodificacion { get; set; }
+        public string HoraRecodificacion { get; set; } = null!;
+        public string Codigo { get; set; } = null!;
+        public string CodigoLaboratorio { get; set; } = null!;
     }
 }
