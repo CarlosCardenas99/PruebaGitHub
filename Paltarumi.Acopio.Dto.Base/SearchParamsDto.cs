841283f623dcf2eaad30e750df13492afbdcfab7
Nelsson José <njose29@outlook.com>
2022-07-02 23:40:57 -0500

    Agregando constructor a al dto de search params

--- Paltarumi.Acopio.Dto.Base/SearchParamsDto.cs
+++ Paltarumi.Acopio.Dto.Base/SearchParamsDto.cs
@@ -4,6 +4,12 @@
     {
         public PageParamsDto? Page { get; set; }
         public IEnumerable<SortParamsDto>? Sort { get; set; }
+
+        public SearchParamsDto()
+        {
+            Page = new PageParamsDto();
+            Sort = new List<SortParamsDto>();
+        }
     }
 
     public class SearchParamsDto<TFilter> : SearchParamsDto
