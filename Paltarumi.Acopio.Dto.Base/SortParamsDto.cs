1831d5c7a7d3db3d15a67ea7e441c6ce421f5184
Nelsson José <njose29@outlook.com>
2022-07-02 22:22:46 -0500

    Agregando constructor al dto de sort params

--- Paltarumi.Acopio.Dto.Base/SortParamsDto.cs
+++ Paltarumi.Acopio.Dto.Base/SortParamsDto.cs
@@ -4,5 +4,17 @@
     {
         public string? Property { get; set; }
         public string? Direction { get; set; }
+
+        public SortParamsDto()
+        {
+            this.Property = null;
+            this.Direction = "desc";
+        }
+
+        public SortParamsDto(string property, string direction)
+        {
+            this.Property = property;
+            this.Direction = direction;
+        }
     }
 }
