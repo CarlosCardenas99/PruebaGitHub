f2ac55f1aa0ed607f63a366ef38da4239620a5f1
yhon6745 <idrissyhonaley@gmail.com>
2022-07-05 18:01:11 -0500

    Configuracion de reportes

--- Paltarumi.Acopio.Balanza.Domain/Mapping/Balanza/LoteBalanzaProfile.cs
+++ Paltarumi.Acopio.Balanza.Domain/Mapping/Balanza/LoteBalanzaProfile.cs
@@ -33,7 +33,7 @@ namespace Paltarumi.Acopio.Balanza.Domain.Mapping.Balanza
                 .ForMember(x => x.NombreConcesion, opt => opt.MapFrom(x => x.IdConcesionNavigation != null ? x.IdConcesionNavigation.Nombre : string.Empty))
                 .ForMember(x => x.NombreProveedor, opt => opt.MapFrom(x => x.IdProveedorNavigation != null ? x.IdProveedorNavigation.RazonSocial : string.Empty))
                 .ForMember(x => x.NombreEstadoTipoMaterial, opt => opt.MapFrom(x => x.IdEstadoTipoMaterialNavigation != null ? x.IdEstadoTipoMaterialNavigation.Descripcion : string.Empty))
-                .ForMember(x => x.NumeroTickets, opt => opt.MapFrom(x => x.Tickets != null ? string.Join(",", x.Tickets.Select(x => x.Numero)) : string.Empty))
+                .ForMember(x => x.NumeroTickets, opt => opt.MapFrom(x => x.Tickets != null ? string.Join(", ", x.Tickets.Select(x => x.Numero)) : string.Empty))
                 .ReverseMap();
 
             CreateMap<Entity.LoteBalanza, SearchLoteBalanzaChecklistDto>()
