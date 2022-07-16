debdd4232ddd467640630757b8bcab1994c389d4
yhon6745 <idrissyhonaley@gmail.com>
2022-07-16 09:44:14 -0500

    se actualizo el search de loteBalanza

--- Paltarumi.Acopio.Balanza.Dto/Balanza/LoteBalanza/SearchLoteBalanzaDto.cs
+++ Paltarumi.Acopio.Balanza.Dto/Balanza/LoteBalanza/SearchLoteBalanzaDto.cs
@@ -1,16 +1,16 @@
-﻿namespace Paltarumi.Acopio.Balanza.Dto.LoteBalanza
+﻿using Paltarumi.Acopio.Balanza.Dto.Ticket;
+
+namespace Paltarumi.Acopio.Balanza.Dto.LoteBalanza
 {
     public class SearchLoteBalanzaDto
     {
         public int IdLoteBalanza { get; set; }
         public string? Estado { get; set; }
-        public string Vehiculos { get; set; } = null!;
         public string? NombreConcesion { get; set; }
         public DateTimeOffset? FechaAcopio { get; set; }
         public string CantidadSacos { get; set; } = null!;
-        public string Transportistas { get; set; } = null!;
         public DateTimeOffset? FechaIngreso { get; set; }
-        public string Codigo { get; set; } = null!;
+        public string CodigoLote { get; set; } = null!;
         public string? NombreProveedor { get; set; }
         public string? NombreEstadoTipoMaterial { get; set; }
         public float Tmh { get; set; }
