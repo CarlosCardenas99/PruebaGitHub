4f32491ec00a0136bccd7c0bf699093ba05d0dc1
Walter Canchan <walter.canchan@somosoh.pe>
2022-07-23 10:14:19 -0500

    empresa

--- Paltarumi.Acopio.Balanza.Dto/Config/Empresa/SelectComboEmpresaDto.cs
+++ Paltarumi.Acopio.Balanza.Dto/Config/Empresa/SelectComboEmpresaDto.cs
@@ -3,5 +3,7 @@ namespace Paltarumi.Acopio.Balanza.Dto.Config.Empresa
 {
     public class SelectComboEmpresaDto: EmpresaDto
     {
+        public int IdEmpresa { get; set; }
+        public bool Activo { get; set; }
     }
 }
