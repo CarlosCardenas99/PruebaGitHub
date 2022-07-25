b67c02a3e10f734a7b4461387208eb9df1717b9c
yhon6745 <idrissyhonaley@gmail.com>
2022-07-24 19:25:14 -0500

    filtros de ticket con vacios

--- Paltarumi.Acopio.Balanza.Domain/Queries/Balanza/Ticket/SearchConsultaTicketQueryHandler.cs
+++ Paltarumi.Acopio.Balanza.Domain/Queries/Balanza/Ticket/SearchConsultaTicketQueryHandler.cs
@@ -31,109 +31,19 @@ namespace Paltarumi.Acopio.Balanza.Domain.Queries.Balanza.Ticket
             Expression<Func<Entity.Ticket, bool>> filter = x => true;
 
             var filters = request.SearchParams?.Filter;
-            //fechas
-            if (filters?.FechaDesde.HasValue == true || filters?.FechaHasta.HasValue == true)
-            {
-                if (filters?.FechaDesde.HasValue == true)
-                {
-                    var fechaDesde = filters.FechaDesde.Value.Date;
-                    filter = filter.And(x => (x.FechaIngreso >= fechaDesde || x.FechaSalida >= fechaDesde));
-                }
 
-                if (filters?.FechaHasta.HasValue == true)
-                {
-                    var fechahasta = filters.FechaHasta.Value.Date.AddDays(1);
-                    filter = filter.And(x => (x.FechaIngreso < fechahasta || x.FechaSalida < fechahasta));
-                }
-            }
-            //codigo
-            if (!string.IsNullOrEmpty(filters?.CodigoLote))
-                filter = filter.And(x => x.IdLoteBalanzaNavigation.CodigoLote.Contains(filters.CodigoLote));
+            filtrarFechas(filters, filter);
+            filtrarCodigoLote(filters, filter);
+            filtrarProveedor(filters, filter);
+            filtrarConcesion(filters, filter);
+            filtrarVehiculo(filters, filter);
+            filtrarConductor(filters, filter);
+            filtrarTara(filters, filter);
+            guiaRemisionRemitente(filters, filter);
+            guiaRemisionTransportista(filters, filter);
+            filtrarTransportista(filters, filter);
+            filtrarEstadoLote(filters, filter);
 
-            //proveedor
-            if (!string.IsNullOrEmpty(filters?.Proveedor))
-            {
-                var proveedores = filters.Proveedor.Split(" ");
-                proveedores.ToList().ForEach(p =>
-                {
-                    filter = filter.And(x =>
-                    (x.IdLoteBalanzaNavigation.IdProveedorNavigation.Ruc.Contains(p) || x.IdLoteBalanzaNavigation.IdProveedorNavigation.RazonSocial.Contains(p)));
-                });
-            }
-            //concesion
-            if (!string.IsNullOrEmpty(filters?.Concesion))
-            {
-                var concesiones = filters.Concesion.Split(" ");
-                concesiones.ToList().ForEach(p =>
-                {
-                    filter = filter.And(x =>
-                    (x.IdLoteBalanzaNavigation.IdConcesionNavigation.CodigoUnico.Contains(p) || x.IdLoteBalanzaNavigation.IdConcesionNavigation.Nombre.Contains(p)));
-                });
-            }
-
-            //vehiculo
-            if (!string.IsNullOrEmpty(filters?.Vehiculo)) 
-            {
-                filters.Vehiculo = filters.Vehiculo.ToUpper();
-                if (filters.Vehiculo.Length == 6) filters.Vehiculo = filters.Vehiculo.Insert(3, "-");
-
-                filter = filter.And(x => x.IdVehiculoNavigation.Placa.Contains(filters.Vehiculo) || x.IdVehiculoNavigation.PlacaCarreta.Contains(filters.Vehiculo));
-            }
-            //conductor
-            if (!string.IsNullOrEmpty(filters?.Conductor))
-                filter = filter.And(x => (x.IdConductorNavigation.Nombres.Contains(filters.Conductor) || x.IdConductorNavigation.Licencia.Contains(filters.Conductor) || x.IdConductorNavigation.Numero.Contains(filters.Conductor)));
-
-            //tara
-            if (filters?.TaraInicial > 0 || filters?.TaraFinal > 0)
-            {
-                if (filters?.TaraInicial > 0)
-                {
-                    var taraInicial = filters.TaraInicial;
-                    filter = filter.And(x => x.Tara >= taraInicial);
-                }
-
-                if (filters?.TaraFinal > 0)
-                {
-                    var taraFinal = filters.TaraFinal;
-                    filter = filter.And(x => x.Tara <= taraFinal );
-                }
-            }
-            //guias
-            if (!string.IsNullOrEmpty(filters?.GuiaRemisionRemitente))
-            {
-                var guias = filters.GuiaRemisionRemitente.Split(" ");
-                guias.ToList().ForEach(p =>
-                {
-                    filter = filter.And(x =>
-                    (x.Grr.Contains(p)));
-                });
-            }
-            //Guia Remitente
-            if (!string.IsNullOrEmpty(filters?.GuiaRemisionTransportista))
-            {
-                var guias = filters.GuiaRemisionTransportista.Split(" ");
-                guias.ToList().ForEach(p =>
-                {
-                    filter = filter.And(x =>
-                    (x.Grt.Contains(p)));
-                });
-            }
-            //estadoLote
-            if (filters?.idEstado.HasValue == true)
-                filter = filter.And(x => x.IdLoteBalanzaNavigation.IdEstado == filters.idEstado);
-
-            //transportista
-            if (!string.IsNullOrEmpty(filters?.Transporte))
-                 filter = filter.And(x => (x.IdTransporteNavigation!.Ruc.Contains(filters.Transporte) || x.IdTransporteNavigation.RazonSocial.Contains(filters.Transporte)));
-
-            //filtros vacios
-            if (filters?.ConductorVacio == true)
-                filter = filter.And(x => x.IdConductor.Value == null);
-
-            if (filters?.TransporteVacio == true)
-                filter = filter.And(x => x.IdTransporte.Value == null);
-
-            //activo
             filter = filter.And(x => x.Activo == true);
 
             var sorts = new List<SortExpression<Entity.Ticket>>();
@@ -189,5 +99,150 @@ namespace Paltarumi.Acopio.Balanza.Domain.Queries.Balanza.Ticket
 
             return await Task.FromResult(response);
         }
+
+        private void filtrarFechas(SearchConsultaTicketFilterDto? filters, Expression<Func<Entity.Ticket, bool>> filter)
+        {
+            if (filters?.FechaDesde.HasValue == true || filters?.FechaHasta.HasValue == true)
+            {
+                if (filters?.FechaDesde.HasValue == true)
+                {
+                    var fechaDesde = filters.FechaDesde.Value.Date;
+                    filter = filter.And(x => (x.FechaIngreso >= fechaDesde || x.FechaSalida >= fechaDesde));
+                }
+
+                if (filters?.FechaHasta.HasValue == true)
+                {
+                    var fechahasta = filters.FechaHasta.Value.Date.AddDays(1);
+                    filter = filter.And(x => (x.FechaIngreso < fechahasta || x.FechaSalida < fechahasta));
+                }
+            }
+        }
+
+        private void filtrarCodigoLote(SearchConsultaTicketFilterDto? filters, Expression<Func<Entity.Ticket, bool>> filter)
+        {
+            if (!string.IsNullOrEmpty(filters?.CodigoLote))
+                filter = filter.And(x => x.IdLoteBalanzaNavigation.CodigoLote.Contains(filters.CodigoLote));
+        }
+
+        private void filtrarProveedor(SearchConsultaTicketFilterDto? filters, Expression<Func<Entity.Ticket, bool>> filter)
+        {
+            if (!string.IsNullOrEmpty(filters?.Proveedor))
+            {
+                var proveedores = filters.Proveedor.Split(" ");
+                proveedores.ToList().ForEach(p =>
+                {
+                    filter = filter.And(x =>
+                    (x.IdLoteBalanzaNavigation.IdProveedorNavigation.Ruc.Contains(p) || x.IdLoteBalanzaNavigation.IdProveedorNavigation.RazonSocial.Contains(p)));
+                });
+            }
+        }
+
+        private void filtrarConcesion(SearchConsultaTicketFilterDto? filters, Expression<Func<Entity.Ticket, bool>> filter)
+        {
+            if (!string.IsNullOrEmpty(filters?.Concesion))
+            {
+                var concesiones = filters.Concesion.Split(" ");
+                concesiones.ToList().ForEach(p =>
+                {
+                    filter = filter.And(x =>
+                    (x.IdLoteBalanzaNavigation.IdConcesionNavigation.CodigoUnico.Contains(p) || x.IdLoteBalanzaNavigation.IdConcesionNavigation.Nombre.Contains(p)));
+                });
+            }
+        }
+
+        private void filtrarVehiculo(SearchConsultaTicketFilterDto? filters, Expression<Func<Entity.Ticket, bool>> filter)
+        {
+            if (filters?.VehiculoVacio == true)
+                filter = filter.And(x => x.IdConductor.Value == null);
+            else
+            {
+                if (!string.IsNullOrEmpty(filters?.Vehiculo))
+                {
+                    filters.Vehiculo = filters.Vehiculo.ToUpper();
+                    if (filters.Vehiculo.Length == 6) filters.Vehiculo = filters.Vehiculo.Insert(3, "-");
+
+                    filter = filter.And(x => x.IdVehiculoNavigation.Placa.Contains(filters.Vehiculo) || x.IdVehiculoNavigation.PlacaCarreta.Contains(filters.Vehiculo));
+                }
+            }
+        }
+        private void filtrarConductor(SearchConsultaTicketFilterDto? filters, Expression<Func<Entity.Ticket, bool>> filter)
+        {
+            if (filters?.ConductorVacio == true)
+                filter = filter.And(x => x.IdConductor.Value == null);
+            else
+            {
+                if (!string.IsNullOrEmpty(filters?.Conductor))
+                    filter = filter.And(x => (x.IdConductorNavigation.Nombres.Contains(filters.Conductor) || x.IdConductorNavigation.Licencia.Contains(filters.Conductor) || x.IdConductorNavigation.Numero.Contains(filters.Conductor)));
+
+            }
+        }
+        private void filtrarTara(SearchConsultaTicketFilterDto? filters, Expression<Func<Entity.Ticket, bool>> filter)
+        {
+            if (filters?.TaraInicial > 0 || filters?.TaraFinal > 0)
+            {
+                if (filters?.TaraInicial > 0)
+                {
+                    var taraInicial = filters.TaraInicial;
+                    filter = filter.And(x => x.Tara >= taraInicial);
+                }
+
+                if (filters?.TaraFinal > 0)
+                {
+                    var taraFinal = filters.TaraFinal;
+                    filter = filter.And(x => x.Tara <= taraFinal);
+                }
+            }
+        }
+        private void guiaRemisionRemitente(SearchConsultaTicketFilterDto? filters, Expression<Func<Entity.Ticket, bool>> filter)
+        {
+            if (filters?.GuiaRemisionRemitenteVacio == true)
+                filter = filter.And(x => x.Grr == string.Empty);
+            else
+            {
+                if (!string.IsNullOrEmpty(filters?.GuiaRemisionRemitente))
+                {
+                    var guias = filters.GuiaRemisionRemitente.Split(" ");
+                    guias.ToList().ForEach(p =>
+                    {
+                        filter = filter.And(x =>
+                        (x.Grr.Contains(p)));
+                    });
+                }
+            }
+        }
+        private void guiaRemisionTransportista(SearchConsultaTicketFilterDto? filters, Expression<Func<Entity.Ticket, bool>> filter)
+        {
+            if (filters?.GuiaRemisionTransportistaVacio == true)
+                filter = filter.And(x => x.Grt == string.Empty);
+            else
+            {
+                if (!string.IsNullOrEmpty(filters?.GuiaRemisionTransportista))
+                {
+                    var guias = filters.GuiaRemisionTransportista.Split(" ");
+                    guias.ToList().ForEach(p =>
+                    {
+                        filter = filter.And(x =>
+                        (x.Grt.Contains(p)));
+                    });
+                }
+            }
+        }
+        private void filtrarTransportista(SearchConsultaTicketFilterDto? filters, Expression<Func<Entity.Ticket, bool>> filter)
+        {
+            if (filters?.TransporteVacio == true)
+                filter = filter.And(x => x.IdTransporte.Value == null);
+            else
+            {
+                if (!string.IsNullOrEmpty(filters?.Transporte))
+                    filter = filter.And(x => (x.IdTransporteNavigation!.Ruc.Contains(filters.Transporte) || x.IdTransporteNavigation.RazonSocial.Contains(filters.Transporte)));
+
+            }
+        }
+        private void filtrarEstadoLote(SearchConsultaTicketFilterDto? filters, Expression<Func<Entity.Ticket, bool>> filter)
+        {
+            if (filters?.idEstado.HasValue == true)
+                filter = filter.And(x => x.IdLoteBalanzaNavigation.IdEstado == filters.idEstado);
+        }
+
     }
 }
--- Paltarumi.Acopio.Balanza.Dto/Balanza/Ticket/SearchConsultaTicketFilterDto.cs
+++ Paltarumi.Acopio.Balanza.Dto/Balanza/Ticket/SearchConsultaTicketFilterDto.cs
@@ -4,19 +4,30 @@
     {
         public DateTimeOffset? FechaDesde { get; set; }
         public DateTimeOffset? FechaHasta { get; set; }
+
         public string? CodigoLote { get; set; }
         public string? Proveedor { get; set; }
         public string? Concesion { get; set; }
+
         public string? Vehiculo { get; set; }
+        public bool VehiculoVacio { get; set; }
+
         public string? Conductor { get; set; }
+        public bool ConductorVacio { get; set; }
+        
+        public string? Transporte { get; set; }
+        public bool TransporteVacio { get; set; }
+
+        public string? GuiaRemisionTransportista { get; set; }
+        public bool GuiaRemisionTransportistaVacio { get; set; }
+
+        public string? GuiaRemisionRemitente { get; set; }
+        public bool GuiaRemisionRemitenteVacio { get; set; }
+
         public int TaraInicial { get; set; }
         public int TaraFinal { get; set; }
-        public string? Transporte { get; set; }
-        public string? GuiaRemisionTransportista { get; set; }
-        public bool TransporteVacio { get; set; }
-        public bool ConductorVacio { get; set; }
-        public string? GuiaRemisionRemitente { get; set; }
-        public bool Activo { get; set; }
+
         public int? idEstado { get; set; }
+        public bool Activo { get; set; }
     }
 }
