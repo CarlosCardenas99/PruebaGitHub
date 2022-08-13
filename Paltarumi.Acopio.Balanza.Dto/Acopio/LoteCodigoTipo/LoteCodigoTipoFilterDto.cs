80422bc6afe20bd01eaec3302e08a2f2f8234b37
Walter Canchan <walter.canchan@somosoh.pe>
2022-08-13 12:20:39 -0500

    tipo lote codigo

--- Paltarumi.Acopio.Balanza.Dto/Acopio/LoteCodigoTipo/LoteCodigoTipoFilterDto.cs
+++
@@ -1,8 +0,0 @@
-namespace Paltarumi.Acopio.Balanza.Dto.Acopio.LoteCodigoTipo
-{
-    public class LoteCodigoTipoFilterDto
-    {
-        public int? IdLoteCodigoTipo { get; set; }
-        public bool? Activo { get; set; }
-    }
-}
--- Paltarumi.Acopio.Balanza.Dto/Acopio/LoteCodigoTipo/SelectComboLoteCodigoTipoFilterDto.cs
+++
@@ -1,8 +0,0 @@
-namespace Paltarumi.Acopio.Balanza.Dto.Acopio.LoteCodigoTipo
-{
-    public class SelectComboLoteCodigoTipoFilterDto
-    {
-        public int? IdLoteCodigoTipo { get; set; }
-        public bool? Activo { get; set; }
-    }
-}
