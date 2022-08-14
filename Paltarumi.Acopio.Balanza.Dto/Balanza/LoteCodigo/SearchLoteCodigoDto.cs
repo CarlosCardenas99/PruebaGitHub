09842b461d587d1f5e831d92db118ac777a40093
yhon6745 <idrissyhonaley@gmail.com>
2022-08-13 19:25:06 -0500

    agregando idlotecodigo

--- Paltarumi.Acopio.Balanza.Dto/Balanza/LoteCodigo/SearchLoteCodigoDto.cs
+++ Paltarumi.Acopio.Balanza.Dto/Balanza/LoteCodigo/SearchLoteCodigoDto.cs
@@ -3,17 +3,19 @@ namespace Paltarumi.Acopio.Balanza.Dto.LoteCodigo
 {
     public class SearchLoteCodigoDto
     {
+        public int IdLoteCodigo { get; set; }
         public DateTimeOffset FechaRecepcion { get; set; }
         public string? loteCodigo { get; set; }
         public string? Proveedor { get; set; }
         public string CodigoPlanta { get; set; } = null!;
         public string CodigoPlantaRandom { get; set; } = null!;
         public string CodigoMuestraProveedor { get; set; } = null!;
+        public string? DuenoMuestra { get; set; }
         public bool EnsayoLeyAu { get; set; }
         public bool EnsayoLeyAg { get; set; }
         public bool EnsayoPorcentajeRecuperacion { get; set; }
         public bool EnsayoConsumo { get; set; }
-        public int IdUsuarioCreate { get; set; }
+        public string? UserNameCreate { get; set; }
         public string Estado { get; set; } = null!;
 
     }
