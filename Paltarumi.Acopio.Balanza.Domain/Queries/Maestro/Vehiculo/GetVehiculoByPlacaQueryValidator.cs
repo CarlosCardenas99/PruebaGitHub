2050532e554bbb2d0b3a931c3f8808e6f1a0231f
yhon6745 <idrissyhonaley@gmail.com>
2022-08-25 16:28:11 -0500

    se agrego la validacion en la busque de vehiculo por placa

---
+++ Paltarumi.Acopio.Balanza.Domain/Queries/Maestro/Vehiculo/GetVehiculoByPlacaQueryValidator.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Paltarumi.Acopio.Balanza.Domain.Queries.Base;
+using Paltarumi.Acopio.Balanza.Repository.Abstractions.Base;
+
+namespace Paltarumi.Acopio.Balanza.Domain.Queries.Maestro.Vehiculo
+{
+    public class GetVehiculoByPlacaQueryValidator : QueryValidatorBase<GetVehiculoByPlacaQuery>
+    {
+        private readonly IRepository<Entity.Vehiculo> _vehiculoRepository;
+
+        public GetVehiculoByPlacaQueryValidator(IRepository<Entity.Vehiculo> vehiculoRepository)
+        {
+            _vehiculoRepository = vehiculoRepository;
+
+            RequiredField(x => x.Placa, Resources.Maestro.Vehiculo.Placa)
+                .DependentRules(() =>
+                {
+                    RuleFor(x => x.Placa)
+                        .MustAsync(ValidateExistenceAsync)
+                        .WithCustomValidationMessage();
+                });
+        }
+
+        protected async Task<bool> ValidateExistenceAsync(GetVehiculoByPlacaQuery command, string placa, ValidationContext<GetVehiculoByPlacaQuery> context, CancellationToken cancellationToken)
+        {
+            placa = placa.Replace(" ", string.Empty);
+            placa = placa.ToUpper();
+            if (placa.Length == 6) placa = placa.Insert(3, "-");
+            
+            var exists = await _vehiculoRepository.FindAll().Where(x => x.Placa.Equals(placa)).AnyAsync(cancellationToken);
+            if (!exists) return CustomValidationMessage(context, Resources.Common.GetRecordNotFound);
+            return true;
+        }
+    }
+}
