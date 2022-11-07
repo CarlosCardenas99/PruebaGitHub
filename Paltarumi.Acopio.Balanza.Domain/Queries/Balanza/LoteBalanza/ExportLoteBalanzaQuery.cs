fd8e35f327668ce0fa283a34f9c462e7f757e07f
Walter Canchan <walter.canchan@somosoh.pe>
2022-11-07 10:41:25 -0500

    update export

--- Paltarumi.Acopio.Balanza.Domain/Queries/Balanza/LoteBalanza/ExportLoteBalanzaQuery.cs
+++ Paltarumi.Acopio.Balanza.Domain/Queries/Balanza/LoteBalanza/ExportLoteBalanzaQuery.cs
@@ -1,7 +1,6 @@
 ﻿using Paltarumi.Acopio.Balanza.Domain.Queries.Base;
 using Paltarumi.Acopio.Balanza.Dto.LoteBalanza;
 using Paltarumi.Acopio.Dto.Base;
-using Paltarumi.Acopio.Export.Application.Dto;
 
 namespace Paltarumi.Acopio.Balanza.Domain.Queries.Maestro.LoteBalanza
 {
